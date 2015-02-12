10a67a606e395965235336ad601008225ba16109
Travis Hooper <travis.hooper@peopleadmin.com>
2015-02-12 14:33:33 -0600

    Creates a styled product page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+Product.create!(
+    [
+      {
+        title: 'Programming Ruby 1.9',
+        description:
+          %{
+            <p>
+              1.9 Ruby is the fastest growing and most exciting dynamic language
+              out there. If you need to get working programs delivered fast,
+              you should add Ruby to your toolbox.
+            </p>
+          },
+        image_url: 'ruby19.jpg',
+        price: 19.00
+      },{
+        title: 'Programming Ruby 2.0',
+        description:
+          %{
+            <p>
+              2.0 Ruby is the fastest growing and most exciting dynamic language
+              out there. If you need to get working programs delivered fast,
+              you should add Ruby to your toolbox.
+            </p>
+          },
+        image_url: 'ruby20.jpg',
+        price: 20.00      
+      }
+    ]
+  )
